18df457fd3272ee5276582f77b081de6005e0737
yeforever <2928297863@qq.com>
2025-01-14 01:26:17 +0800

    测试

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class Program
+    {
+
+        /// <summary>
+        /// 这个是程序的主入口
+        /// </summary>
+        /// <param name="args">这个参数没有一点用处</param>
+        static void Main(string[] args)
+        {
+            #region
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine();
+            Console.Write("Hello,World");
+            Console.ReadLine();
+            
+
+            Console.Read();
+            Console.Write("hello");
+            Console.Read();
+            Console.Write("hello");
+            #endregion
+        }
+    }
+}
