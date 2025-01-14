14431b74c8d7934bd0888c4d541f895cadcc5642
yeforever <2928297863@qq.com>
2025-01-15 02:14:35 +0800

    更新

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -25,6 +25,28 @@
             Console.Read();
             Console.Write("hello");
             #endregion
+
+            //decimal:(金钱小数)存储金钱
+            //浮点数
+            //1.他要确定有效的范围
+            //2.他会有精度损失
+            //3.他性能消耗很大
+            //4.decimal 和其他类型不包含血缘关系
+            // decimal c=1.1m//decimal后面必须带m  M
+            //char
+            //    它什么值都可以存
+            //    他的值必须写在’’里面
+            //    也可以不用写在’’   但必须对应ascall码
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
